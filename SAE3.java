31e50054ce2a2e471c80ef9e5a03b30f6a84719e
James Morbz <jamesmoreby1@gmail.com>
2021-11-17 10:36:14 +0000

    Fixing SAE3

--- SAE3.java
+++ SAE3.java
@@ -2,23 +2,74 @@ import java.util.Scanner;
 
 public class SAE3 {
     
-    public static boolean isDisabled() {
+    public static void isDisabled() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Are you disabled? ");
+        System.out.print("Are you disabled? (yes/no) ");
         String isDisabled = in.nextLine();
         if (isDisabled.toLowerCase().equals("yes")) {
-            return true;
-        } else if (isDisabled.toLowerCase().equals("no")) {
-            return false;
+            System.out.println("Parking for you is free");
+        } else if (isDisabled.toLowerCase().equals("no")){
+            CostCorrection();
         } else {
+            System.out.println("Please answer \"yes\" or \"no\"");
             isDisabled();
-            return true;
-        } 
+        }
     }
 
+    public static double MainLoop() {
+        double cost = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("How many hours have you parked for? (1-8)");
+        int hours = in.nextInt();
+        if (hours == 1) {
+            cost = 3.00;
+            return cost;
+        } else if (2<=hours && hours<=4) {
+            cost = 4.00;
+            return cost;
+        } else if (5<=hours && hours<=6) {
+            cost = 4.50;
+            return cost;
+        } else if (7<=hours && hours<=8) {
+            cost = 5.50;
+            return cost;
+        } else {
+            System.out.println("Enter an input within range...");
+            return MainLoop();
+        }
+
+    }
+    public static void CostCorrection() {
+        Scanner in = new Scanner(System.in);
+        boolean LocalBool;
+        boolean OAPBool;
+        double cost = MainLoop();
+        System.out.print("Do you have an \"I live Locally badge\"? (yes/no) ");
+        String Local = in.nextLine();
+        System.out.print("Are you an OAP? (yes/no) ");
+        String OAP = in.nextLine();
+
+        if (Local.toLowerCase().equals("yes")) {
+            LocalBool = true;
+        } else {
+            LocalBool = false;
+        }
+        if (OAP.toLowerCase().equals("yes")) {
+            OAPBool = true;
+        } else {
+            OAPBool = false;
+        }
+        
+        if (LocalBool == true) {
+            cost = cost - 1.00;
+        }
+        if (OAPBool == true) {
+            cost = cost - 2.00;
+        }
+        System.out.println("The parking charge for you is " + cost + " pounds.");
+    }
     public static void main(String[] args) {
-        boolean disabledBool = isDisabled();
-        System.out.println(disabledBool);
+        isDisabled();
         
     }
 }
