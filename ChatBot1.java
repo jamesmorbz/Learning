481b91a768f2983cf28336986a28d31396d78e8d
James Morbz <jamesmoreby1@gmail.com>
2021-11-20 19:22:32 +0000

    Adding First Stage of Chatbot

--- ChatBot1.java
+++ ChatBot1.java
@@ -1,14 +1,22 @@
+/* ***************************************
+  Name : James Moreby
+  Date : 20/11/2021
+  Program: ChatBotv1
+  Stage of Prod: Level 1
+  
+*************************************** */
+
 import java.util.*;
 
 public class ChatBot1 {
-    public static void newBeginnings() {
+    public static void QuickChat() {
         Scanner in = new Scanner(System.in);
         System.out.println("Hi!");
-        String line = in.nextLine();
+        String firstUserInput = in.nextLine();
         System.out.println("It's nice to meet you! I need to run though!");
     }
     public static void main(String[] args) {
-        newBeginnings();
+        QuickChat();
     }
     
 }
