0025f6db3cc8587175d5da68c01043ae3519d6dc
James Morbz <jamesmoreby1@gmail.com>
2021-11-20 19:20:02 +0000

    Adding First Stage of Chatbot

---
+++ ChatBot1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ChatBot1 {
+    public static void newBeginnings() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hi!");
+        String line = in.nextLine();
+        System.out.println("It's nice to meet you! I need to run though!");
+    }
+    public static void main(String[] args) {
+        newBeginnings();
+    }
+    
+}
