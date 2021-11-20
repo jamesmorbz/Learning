4723c8b67c1012e701407f71ce141ae780122c4a
James Morbz <jamesmoreby1@gmail.com>
2021-11-20 00:13:18 +0000

    Adding T Functionality + Error Handling

--- SAE8.java
+++ SAE8.java
@@ -4,7 +4,14 @@ public class SAE8 {
     
     private static long getPolishSum(String[] input) {
         Stack<String> stackOfChars = new Stack<String>();
-        for (int i =input.length-1; i > -1; i--) {
+        for (int i=input.length-1; i > -1; i--) {
+            if (!(input[i].equals("1") ||input[i].equals("2")||input[i].equals("3") ||input[i].equals("4")||input[i].equals("5")
+            ||input[i].equals("6")||input[i].equals("7")||input[i].equals("8")||input[i].equals("9")
+            ||input[i].equals("A")||input[i].equals("B")||input[i].equals("C")
+            ||input[i].equals("T")||input[i].equals("*"))) {
+                System.out.println("Input contained invalid characters...Program Closing");
+                System.exit(1);
+            }
             if (input[i].equals("A")) {
                 input[i] = "10";
             }
@@ -14,17 +21,17 @@ public class SAE8 {
             if (input[i].equals("C")) {
                 input[i] = "12";
             }
+            if (input[i].equals("T")) {
+                long firstValue = Long.valueOf(stackOfChars.pop());
+                Long T = firstValue + firstValue+1 + firstValue+2;
+                input[i] = Long.toString(T);
+            }
             if (input[i].equals("*")) {
                 long firstValue = Long.valueOf(stackOfChars.pop());
-                System.out.println(firstValue);
                 long secondValue = Long.valueOf(stackOfChars.pop());
-                System.out.println(secondValue);
-                long holdingValue = 0;
-                if (input[i].equals("*")) {
-                holdingValue = firstValue * secondValue;
-                System.out.println(holdingValue);
-                stackOfChars.push(String.valueOf(holdingValue));
-                }
+                long newValueAfterCalc = 0;
+                newValueAfterCalc = firstValue * secondValue;
+                stackOfChars.push(String.valueOf(newValueAfterCalc));
             }
             else stackOfChars.push(input[i]);
         }
@@ -36,7 +43,7 @@ public class SAE8 {
         System.out.print("Please Enter the Expression: ");
         String input = in.nextLine();
         String[] list = input.split("");
-        System.out.println(Arrays.toString(list));      
-        System.out.println(getPolishSum(list));
+        Long answer = getPolishSum(list);
+        System.out.println("The answer is " + answer);
     }                                                       
 }
