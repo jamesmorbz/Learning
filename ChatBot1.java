ff0f5103f2bf099d6d84b9c4c9fc2df78f2ce6ea
James Morbz <jamesmoreby1@gmail.com>
2021-11-20 21:00:25 +0000

    Changing ChatBot v1 for Spec

--- ChatBot1.java
+++ ChatBot1.java
@@ -11,12 +11,16 @@ import java.util.*;
 public class ChatBot1 {
     public static void QuickChat() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hi!");
+        System.out.println("Bot: Hi!");
+        System.out.print("<Unknown User>: ");
         String firstUserInput = in.nextLine();
+    }
+    public static void GTG() {
         System.out.println("It's nice to meet you! I need to run though!");
     }
     public static void main(String[] args) {
         QuickChat();
+        GTG();
     }
     
 }
