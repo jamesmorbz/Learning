92c2ae46c4e89cbcf6f2824412ca8275fbf1bae9
James Morbz <jamesmoreby1@gmail.com>
2022-01-23 17:36:11 +0000

    Adding Final Exam, Python Multithreading Examples

---
+++ FinalExam/Question1.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class Question1 {
+
+    public static int testMultiplication(int numberOfQuestions, int yMax, int xMin, int xMax) {
+        Scanner in = new Scanner(System.in);
+        int numberOfCorrectAnswers = 0; // counter for correct answers
+        Random r = new Random();
+        for (int i = 0; i<numberOfQuestions; i++) {
+            int randomX = r.nextInt(xMax-xMin+1)+xMin; 
+            int randomY = r.nextInt(yMax)+1; 
+            int correctAnswer = randomX * randomY;
+            System.out.printf("What is %d * %d ? ", randomX , randomY);
+            int userAnswer = in.nextInt();
+            if (correctAnswer == userAnswer){
+                System.out.println("Correct."); 
+                numberOfCorrectAnswers ++; // increments the counter by 1
+            }
+            else{
+                System.out.println("Incorrect.");
+            }
+        }
+        return numberOfCorrectAnswers;
+    }
+
+    public static int calculatePercentage(int correctAnswers, int numberOfQuestions) {
+        return Math.round(((float)correctAnswers / (float)numberOfQuestions) * 100); // calculating percentage casting to ints to mantain accuracy
+    }
+
+    public static char getGrade(int percentage) {
+        if (percentage>=70) { // if else statments are the boundaries for the grades specified in the question
+            return 'A';
+        } else if (percentage>=60) {
+            return 'B';
+        } else if (percentage>=50) {
+            return 'C';
+        } else if (percentage>=40) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+
+    public static int getStartInput() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Minimum X Value: ");
+        int xMin = in.nextInt();
+        if (xMin < 1) {
+            System.out.println("Min X value must be greater than 0");
+            return getStartInput();
+        }
+        return xMin;
+    }
+
+    public static int getEndInput(int xMin) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Maximum X Value: ");
+        int xMax = in.nextInt();
+        if (xMax < xMin) {
+            System.out.println("Max X value must be greater or equal to Min X value");
+            return getEndInput(xMin);
+        }
+        return xMax;
+    }
+
+    public static void printSummary(int numberOfCorrectAnswers, int numberOfQuestions, int percentage, char grade) {
+        System.out.printf("You scored %d/%d (%d%c) %c grade.", numberOfCorrectAnswers, numberOfQuestions, percentage, '%', grade);
+    }
+    public static void main(String[] args) {
+        
+        final int numberOfQuestions = 12; // final variable for number of questions that will be asked
+        final int yMax = 12; // maximum times tables 
+        final int xMin = getStartInput(); // user min val input
+        final int xMax = getEndInput(xMin); // user max val input, checks against min to make sure max>=min
+
+        int numberOfCorrectAnswers = testMultiplication(numberOfQuestions, yMax, xMin, xMax);
+        int percentage = calculatePercentage(numberOfCorrectAnswers, numberOfQuestions); // calculating quiz percentage
+        char grade = getGrade(percentage); // grading the quiz
+
+        printSummary(numberOfCorrectAnswers, numberOfQuestions, percentage, grade); // printing summmary of quiz result
+        
+    }
+}
+
---
+++ FinalExam/Question2.java
@@ -0,0 +1,96 @@
+import java.util.*;
+
+public class Question2 {
+
+    static class Voter {
+        String name;
+        int votes;
+    }
+
+    public static Voter createVoter(String name) {
+        Voter nw = new Voter();
+        nw.name = name;
+        nw.votes = 0;
+        return nw;
+    }
+
+    public static String getVoterName(Voter nw) {
+        return nw.name;
+    }
+
+    public static int getNumVotes(Voter nw) {
+        return nw.votes;
+    }
+
+    public static void incrementVotes(Voter nw) {
+        nw.votes = nw.votes + 1;
+    }
+
+    public static String[] getParticipants(int numberOfParticipants) {
+        Scanner in = new Scanner(System.in);
+        int j = 0;
+        String[] participants = new String[10];
+        for (int i = 1; i <= numberOfParticipants; i++) {
+            System.out.printf("Name %d? ", i);
+            String name = in.nextLine();
+            participants[i - 1] = name;
+            createVoter(name);
+        }
+        System.out.print("Voting: ");
+        while (j < numberOfParticipants) {
+            System.out.printf("%d=%s ", j + 1, participants[j]);
+            j++;
+        }
+        System.out.println();
+        return participants;
+    }
+
+    public static int[] voteCounter(String[] participants) {
+        Scanner in = new Scanner(System.in);
+        int[] VotesCounted = new int[10];
+        for (int i = 0; i < participants.length; i++) {
+            System.out.printf("%s who do you vote for? ", participants[i]);
+            int VotedFor = in.nextInt();
+            if ((VotedFor < 1) || (VotedFor > 10)) {
+                System.out.println("Invalid Voting Number");
+                i--;
+            } else {
+                VotesCounted[i] = VotedFor;
+            }
+        }
+        return VotesCounted;
+    }
+
+    public static int findMostPopular(int[] votes) {
+        Arrays.sort(votes);
+
+        int previous = votes[0];
+        int popular = votes[0];
+        int currentCount = 1;
+        int highestCount = 1;
+
+        for (int i = 1; i < votes.length; i++) {
+            if (votes[i] == previous)
+                currentCount++;
+            else {
+                if (currentCount > highestCount) {
+                    popular = votes[i - 1];
+                    highestCount = currentCount;
+                }
+                previous = votes[i];
+                currentCount = 1;
+            }
+        }
+
+        return currentCount > highestCount ? votes[votes.length - 1] : popular;
+
+    }
+
+    public static void main(String[] args) {
+        final int numberOfParticipants = 10;
+        String[] participants = getParticipants(numberOfParticipants);
+        int[] votes = voteCounter(participants);
+        int mostPopular = findMostPopular(votes);
+        System.out.printf("Winner(s): %s", participants[mostPopular-1]);
+    }
+}
